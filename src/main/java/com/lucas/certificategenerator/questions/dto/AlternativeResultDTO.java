efe992743eba5e782c9252ccaa18b03c1843b50a
lucasmoraist <luksmn1101@gmail.com>
2024-02-06 15:53:52 -0300

    feat: criando dtos com dados que serão recuperados quando chamar a requisição

---
+++ src/main/java/com/lucas/certificategenerator/questions/dto/AlternativeResultDTO.java
@@ -0,0 +1,17 @@
+package com.lucas.certificategenerator.questions.dto;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AlternativeResultDTO {
+    private UUID id;
+    private String description;
+}
