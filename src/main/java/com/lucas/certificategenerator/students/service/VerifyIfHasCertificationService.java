186ba8c27178d2c570b451258d1df570e2dfb12b
lucasmoraist <luksmn1101@gmail.com>
2024-02-06 13:42:04 -0300

    feat: fazendo verificação dentro do bd através do repository

--- src/main/java/com/lucas/certificategenerator/students/service/VerifyIfHasCertificationService.java
+++ src/main/java/com/lucas/certificategenerator/students/service/VerifyIfHasCertificationService.java
@@ -3,12 +3,21 @@ package com.lucas.certificategenerator.students.service;
 import org.springframework.stereotype.Service;
 
 import com.lucas.certificategenerator.students.dto.VerifyHasCertificationDTO;
+import com.lucas.certificategenerator.students.repository.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationService {
-    
-    public boolean execute(VerifyHasCertificationDTO dto){
-        if(dto.email().equals("lucas@lucas.com") && dto.technology().equals("JAVA")){
+
+    private final CertificationStudentRepository repository;
+
+    public VerifyIfHasCertificationService(CertificationStudentRepository repository) {
+        this.repository = repository;
+    }
+
+    public boolean execute(VerifyHasCertificationDTO dto) {
+        var result = this.repository.findByEmailAndTechnology(dto.email(), dto.technology());
+
+        if (!result.isEmpty()) {
             return true;
         }
         return false;
