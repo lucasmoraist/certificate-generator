bd38162793b1f3ab74f810815281dacbc5d53d2a
lucasmoraist <luksmnt1101@gmail.com>
2024-02-05 21:58:34 -0300

    feat: esqueleto do projeto

---
+++ src/main/java/com/lucas/certificategenerator/CertificateGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.certificategenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificateGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/certificategenerator/CertificateGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.certificategenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
